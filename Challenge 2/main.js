f6868d05f2e3372cd39102c6688a6ee6ea27545a
appsec01ey <appsec01.ey@gmail.com>
2025-02-09 12:04:35 +0530

    Update main.js

--- Challenge 2/main.js
+++ Challenge 2/main.js
@@ -36,5 +36,13 @@ app.listen(3000, () => {
 });
 
 /* Solution 
-Here we can see that in line 21 the application takes url from the ?url param in the query and then sends the response data leading to potential SSRF 
+1. Here we can see that in line 21 the application takes url from the ?url param in the query , queries the url (no validation / whitelisting etc) and then sends the response data leading to potential SSRF 
+2. Example : http://127.0.0.1:3000/fetch-data/?url=http://127.0.0.1:3000/profile 
+3. Here implement follow checks:
+    1. isValidUrl : checks if url is valid or not
+    2. create a list of allowed domains and only allow those urls 
+    3. if whole url is not required to be dynamic then use regex to validate the remaining part
+    4. A sample solution is provided in solution.js file
+
+*/
 
