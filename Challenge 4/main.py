975c42e1932450a52c238366a4b1c26ad87b9794
appsec01ey <appsec01.ey@gmail.com>
2025-02-10 21:15:37 +0530

    Update main.py

--- Challenge 4/main.py
+++ Challenge 4/main.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
     app.run(debug=False)
 
 
-/*
+"""
 1. Here the key issue is hardcoded credentials so try to store creds in a database (password should be hashed using bcrypt or argon n stored)
 2. If creds are used to relay to some other app or service then : 
   1. Store the secret in an environment variable or a restricted config file (that is not part of version control)
   2. key vaults like google secret manager or aws secret vault etc
-*/
+"""
