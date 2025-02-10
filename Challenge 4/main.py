9b0c1c5f6bbdab794703d8ace73522a8901316fb
appsec01ey <appsec01.ey@gmail.com>
2025-02-10 21:22:03 +0530

    Update main.py

--- Challenge 4/main.py
+++ Challenge 4/main.py
@@ -30,4 +30,5 @@ if __name__ == '__main__':
 2. If creds are used to relay to some other app or service then : 
   1. Store the secret in an environment variable or a restricted config file (that is not part of version control)
   2. key vaults like google secret manager or aws secret vault etc
+3. Additionally we can use @limiter.limit i.e flask limiter module to add rate limit to login endpoint n prevent bruteforcing attacks.
 """
